bedd960acc91d35204fca2050a13232a0f100fb3
hazadus <hazadus7@gmail.com>
2025-02-25 16:45:17 +0300

    Add form validation and reset

--- src/form.js
+++ src/form.js
@@ -8,8 +8,42 @@ document.addEventListener("alpine:init", () => {
     bio: '',
     newsletter: false,
     showPassword: false,
-    submitForm() {
-      alert('Form submitted!');
+
+    errors: {},
+    validateForm(){
+      this.errors = {};
+      
+      if (this.username.length < 3) {
+        this.errors.username = 'Username must be at least 3 characters long';
+      }
+      if (this.password.length < 6) {
+        this.errors.password = 'Password must be at least 6 characters long';
+      }
+      if (this.password !== this.passwordConfirm) {
+        this.errors.passwordConfirm = 'Passwords do not match';
+      }
+      if (!this.color) {
+        this.errors.color = 'Please select a color';
+      }
+      if (this.bio.length < 10) {
+        this.errors.bio = 'Bio must be at least 10 characters long';
+      }
+    },
+
+    // В качестве параметра может быть передано событие $event
+    submitForm($event) {
+      this.validateForm();
+      
+      if (Object.keys(this.errors).length > 0) {
+        alert('❌ Form has errors');
+        console.log(this.errors);
+        return
+      }
+
+      alert('✅ Form submitted!');
+      // В данном случае событие будет submit, а цель - форма
+      // Очистим форму после "отправки" данных
+      $event.target.reset();
     }
   }));
 });
