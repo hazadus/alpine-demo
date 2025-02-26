37cd84a23131a0df99c278aa972e83064e375563
hazadus <hazadus7@gmail.com>
2025-02-26 22:05:59 +0300

    Add  demo

--- src/fetch.js
+++ src/fetch.js
@@ -2,12 +2,19 @@ document.addEventListener("alpine:init", () => {
   // NB: скобки после => обязательны, чтобы вернуть объект
   Alpine.data('demo', () => ({
     posts: [],
+    color: '',
 
     // Метод, который будет вызван при инициализации компонента
     init() {
       fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
         .then(response => response.json())
         .then(data => this.posts = data);
+
+      // Устанавливаем слежение за изменениями переменной color
+      this.$watch('color', (newColor, oldColor) => {
+        console.log(`Color changed from ${oldColor} to ${newColor}`);
+        document.body.style.backgroundColor = newColor;
+      });
     },
   }));
 });
